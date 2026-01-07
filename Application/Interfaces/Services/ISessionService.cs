46dfd7771a8cba032ecef42bffedc31ef8b83602
okikiola2010 <bakareumar06@gmail.com>
2026-01-07 15:33:16 +0100

    umar

---
+++ Application/Interfaces/Services/ISessionService.cs
@@ -0,0 +1,17 @@
+﻿using Application.Dtos;
+using Domain.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Application.Interfaces.Services
+{
+    public interface ISessionService
+    {
+        Task AddSession(SessionRequestModel model);
+        Task Delete(Guid Id);
+        Task<List<Session>> GetAllSessions();
+    }
+}
