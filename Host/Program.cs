ad97d70e7da7f9ae0ccc7ed82b954b25459a5aaf
ProfessorYomi <abdulqauyumayomiposi652@gmail.com>
2026-01-07 11:40:05 +0100

    Appdbcontext#

--- Host/Program.cs
+++ Host/Program.cs
@@ -1,3 +1,5 @@
+using Infrastructure.Context;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -6,6 +8,7 @@ builder.Services.AddControllers();
 // Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
 builder.Services.AddOpenApi();
 builder.Services.AddSwaggerGen();
+builder.Services.AddSingleton<AppDbContext>();
 
 var app = builder.Build();
 
--- Infrastructure/Context/AppDbContext.cs
+++ Infrastructure/Context/AppDbContext.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.EntityFrameworkCore;
+﻿using Domain.Entities;
+using Microsoft.EntityFrameworkCore;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -10,6 +11,30 @@ namespace Infrastructure.Context
     public class AppDbContext : DbContext
     {
         public AppDbContext(DbContextOptions<AppDbContext> opts) : base(opts)
-        {}
+        {
+
+        }
+        public DbSet<User> Users { get; set; }
+        public DbSet<Student> Students { get; set; }
+        public DbSet<Department> Departments { get; set; }
+        public DbSet<Session> Sessions { get; set; }
+        public DbSet<ExamType> ExamTypes { get; set; }
+        public DbSet<StudentExamType> StudentExamTypes {  get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+            modelBuilder.Entity<User>().HasData(
+                new User
+                {
+                    UserName = "admin@gmail.com",
+                    HashPassword = "admin123",
+                    Salt = "",
+                    Role = "Admin"
+                }
+                );
+
+        }
     }
+    
 }
