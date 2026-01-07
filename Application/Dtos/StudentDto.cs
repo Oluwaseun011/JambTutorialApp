8dd87c34a9a57572cb67d6961af92b0273bcef23
Haseenah-Sho <haseenahshoboyede@gmail.com>
2026-01-07 14:57:15 -0800

    student dto

---
+++ Application/Dtos/StudentDto.cs
@@ -0,0 +1,42 @@
+﻿using Domain.Entities;
+using Domain.Enums;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Application.Dtos
+{
+    public class StudentDto
+    {
+        public Guid Id { get; set; } = Guid.NewGuid();
+        public string UserName { get; set; } = default!;
+        public string HashPassword { get; set; } = default!;
+        public string FirstName { get; set; } = default!;
+        public string LastName { get; set; } = default!;
+        public string Email { get; set; } = default!;
+        public string PhoneNumber { get; set; } = default!;
+        public Gender Gender { get; set; }
+        public DateTime Dob { get; set; }
+    }
+
+    public class RegisterStudentRequestModel
+    {
+        public string UserName { get; set; } = default!;
+        public string FirstName { get; set; } = default!;
+        public string LastName { get; set; } = default!;
+        public string Email { get; set; } = default!;
+        public string HashPassword { get; set; } = default!;
+        public string PhoneNumber { get; set; } = default!;
+        public Gender Gender { get; set; }
+        public DateTime Dob { get; set; }
+    }
+
+    public class RegisterStudentResponseModel
+    {
+        public Guid Id { get; set; }
+        public string FirstName { get; set; } = default!;
+        public string LastName { get; set; } = default!;
+    }
+}
