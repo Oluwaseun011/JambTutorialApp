4ee559f58d13e425429a0144d55e14c15e6053ea
sultaan0x <sultaanjamiuazeez@gmail.com>
2026-01-07 11:33:50 +0100

    dtos

---
+++ Application/Dtos/BaseResponse.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Application.Dtos
+{
+    public class BaseResponse<T>
+    {
+        public bool Status { get; set; }
+        public string Message { get; set; } = default!;
+        public T? Data { get; set; }
+    }
+}
---
+++ Application/Dtos/ExamTypeDto.cs
@@ -0,0 +1,20 @@
+﻿using Domain.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Application.Dtos
+{
+    public class ExamTypeDto
+    {
+        public Guid Id { get; set; } = Guid.NewGuid();
+        public string Name { get; set; } = default!;
+        public string Description { get; set; } = default!;
+        public decimal Price { get; set; }
+        public ICollection<StudentExamType> StudentExamTypes { get; set; } = new HashSet<StudentExamType>();
+    }
+    public record ExamTypeRequestModel(string Name, string Description, decimal Price);
+    public record ExamTypeResponseModel(Guid Id, string Name);
+}
