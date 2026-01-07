4283fe0dcf1f101c62ebcbcfc8510d673ca6c966
sultaan0x <sultaanjamiuazeez@gmail.com>
2026-01-07 15:29:23 +0100

    IDepartmentService

---
+++ Application/Interfaces/Services/IDepartmentService.cs
@@ -0,0 +1,17 @@
+﻿using Domain.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Application.Interfaces.Services
+{
+    public interface IDepartmentService
+    {
+        public void Add(Department department);
+        public Department GetDepartment(Guid id);
+        public ICollection<Department> GetAllDepartment();
+        public ICollection<Department> GetDepartmentsByExamType(Guid examTypeId);
+    }
+}
