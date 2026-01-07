cd371358828d848c99921c3e6f0642b08ceaf7b5
KawtharKay <kawtharkehinde@gmail.com>
2026-01-07 11:37:28 +0100

    ExamType And UnitOfWork Repo

---
+++ Application/Interfaces/Repositories/IExamTypeRepository.cs
@@ -0,0 +1,16 @@
+﻿using Domain.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Application.Interfaces.Repositories
+{
+    public interface IExamTypeRepository 
+    {
+        Task AddAsync(ExamType examType);
+        Task<ExamType> GetAsync(Guid id);
+        Task<ICollection<ExamType>> GetAllAsync();
+    }
+}
