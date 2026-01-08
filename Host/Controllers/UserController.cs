f278e40eab22695694bc27ec410d4aea834c8984
Absurd-X-X <ajibikeabdulsamad@gmail.com>
2026-01-08 19:05:19 +0100

    LogInDone

---
+++ Host/Controllers/UserController.cs
@@ -0,0 +1,33 @@
+﻿using Application.Dtos;
+using Application.Interfaces.Services;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Host.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class UserController : ControllerBase
+    {
+        private readonly IUserService _userService;
+
+        public UserController(IUserService userService)
+        {
+            _userService = userService;
+        }
+
+        [HttpGet]
+
+        public async Task<IActionResult> Login(LoginRequestModel model)
+        {
+            BaseResponse<LoginResponseModel> response = await _userService.Login(model);
+
+            if (response.IsSuccessful == null)
+            {
+                return BadRequest();
+            }
+
+            return Ok(response);
+        }
+    }
+}
