6340859e2fec5e4ce14850bd6941ba283e69bdd0
Haseenah-Sho <haseenahshoboyede@gmail.com>
2026-01-08 17:25:54 -0800

    ExamTypeServiceCache

--- Application/Constants/CacheKeys.cs
+++ Application/Constants/CacheKeys.cs
@@ -9,5 +9,6 @@ namespace Application.Constants
     public class CacheKeys
     {
         public const string all_departments = "departments";
+        public const string all_examTypes = "examTypes";
     }
 }
--- Application/Services/ExamTypeService.cs
+++ Application/Services/ExamTypeService.cs
@@ -1,7 +1,9 @@
-﻿using Application.Dtos;
+﻿using Application.Constants;
+using Application.Dtos;
 using Application.Interfaces.Repositories;
 using Application.Interfaces.Services;
 using Domain.Entities;
+using Microsoft.Extensions.Caching.Memory;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -13,14 +15,19 @@ namespace Application.Services
     public class ExamTypeService : IExamTypeService
     {
         private readonly IExamTypeRepository _examTypeRepository;
+        private readonly IMemoryCache _memoryCache;
         private readonly IUnitOfWork _unitOfWork;
-        public ExamTypeService(IExamTypeRepository examTypeRepository, IUnitOfWork unitOfWork)
+        public ExamTypeService(IExamTypeRepository examTypeRepository, IMemoryCache memoryCache, IUnitOfWork unitOfWork)
         {
             _examTypeRepository = examTypeRepository;
+            _memoryCache = memoryCache;
             _unitOfWork = unitOfWork;
         }
-        public async Task<BaseResponse<ExamTypeResponseModel>> Create(ExamTypeRequestModel model)
+
+        public async Task<BaseResponse<Guid>> Create(CreateExamTypeRequestModel model)
         {
+            var examTypeExist = await _examTypeRepository.IsExistAsync(model.Name);
+            if (examTypeExist) return BaseResponse<Guid>.Failure("Already exist");
             var examType = new ExamType
             {
                 Name = model.Name,
@@ -29,59 +36,43 @@ namespace Application.Services
             };
             await _examTypeRepository.AddAsync(examType);
             await _unitOfWork.SaveAsync();
-            return new BaseResponse<ExamTypeResponseModel>
-            {
-                Status = true,
-                Data = new ExamTypeResponseModel
-                {
-                    Id = examType.Id,
-                    Name = examType.Name
-                }
 
-            };
+            _memoryCache.Remove(CacheKeys.all_examTypes);
+            return BaseResponse<Guid>.Success(examType.Id, "Created Successfully");
 
         }
 
-        public async Task<BaseResponse<ICollection<ExamTypeDto>>> GetAllExamTypes()
+        public async Task<BaseResponse<ExamTypeDto?>> GetExamType(Guid id)
         {
-            var listOfExamType = await _examTypeRepository.GetAllAsync();
-            if (listOfExamType is null)
+            var examType = await _examTypeRepository.GetAsync(id);
+            if (examType is null)return BaseResponse<ExamTypeDto?>.Failure("Not found");
+
+            var dto = new ExamTypeDto
             {
-                return new BaseResponse<ICollection<ExamTypeDto>>
-                {
-                    Status = false,
-                    Data = null
-                };
-                
-             }
-            return new BaseResponse<ICollection<ExamTypeDto>>
+                Id = examType.Id,
+                Name = examType.Name,
+                Description = examType.Description,
+                Price = examType.Price
+            };
+            return BaseResponse<ExamTypeDto?>.Success(dto, "successful");
+        }
+
+        public async Task<BaseResponse<IEnumerable<ExamTypeDto?>>> GetExamTypes()
+        {
+            if(!_memoryCache.TryGetValue(CacheKeys.all_examTypes, out IEnumerable<ExamTypeDto?> examTypes))
             {
-                Status = true,
-                Data = listOfExamType.Select(x => new ExamTypeDto
+                var allExamTypes = await _examTypeRepository.GetAllAsync();
+                examTypes = allExamTypes.Select(x => new ExamTypeDto
                 {
                     Id = x.Id,
-                    Name = x.Name
-                }).ToList()
-            };
-        }
+                    Name = x.Name,
+                    Description = x.Description,
+                    Price = x.Price
+                }).ToList();
 
-        public async Task<BaseResponse<ExamTypeDto>> GetExamType(Guid id)
-        {
-            var response = await _examTypeRepository.GetAsync(id);
-            if (response is null) return new BaseResponse<ExamTypeDto>
-            {
-                Status = false,
-                Data = null
-            };
-            return new BaseResponse<ExamTypeDto>
-            {
-                Status = true,
-                Data = new ExamTypeDto
-                {
-                    Id = id,
-                    Name = response.Name
-                }
-            };
+                _memoryCache.Set(CacheKeys.all_examTypes, examTypes);
+            }
+            return BaseResponse<IEnumerable<ExamTypeDto?>>.Success(examTypes, "successful");
         }
     }
 }
